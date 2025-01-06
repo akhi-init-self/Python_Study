c41a216485c0417ccfa0e42e7aace9581612da03
Tulip2MF <tulip2mf@gmail.com>
2025-01-06 20:18:02 +0100

    turtle color added

---
+++ day-16-start/main.py
@@ -0,0 +1,12 @@
+import turtle
+
+timmy = turtle.Turtle()
+timmy.shape("turtle")
+timmy.color("yellow4")
+my_screen = turtle.Screen()
+my_screen.canvheight =500
+my_screen.canvwidth =500
+
+my_screen.exitonclick()
+
+print(my_screen)
